47e27a7adf4f390b71a661401ff61ad4eb483f6b
Patel <jay.patel@vaimanagement.co>
2023-03-10 11:16:18 +0400

    added changes

--- components/blogsection/podcastImageComponent.js
+++ components/blogsection/podcastImageComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "../../styles/podcastImage.module.scss"
+import styles from "../../styles/podcastimage.module.scss"
 
 const PodcastImageComponent = () => {
   return (
